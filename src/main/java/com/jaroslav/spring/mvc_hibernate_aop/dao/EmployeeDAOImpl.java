fcbe531391dd2b86931140b833a745bda6edf8bf
Jaroslav <monatkotata@gmail.com>
2022-07-21 17:23:34 +0300

    add service EmployeeServiceImpl

--- src/main/java/com/jaroslav/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ src/main/java/com/jaroslav/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -16,8 +16,7 @@ public class EmployeeDAOImpl implements EmployeeDAO{
     private SessionFactory sessionFactory;
 
     @Override
-    @Transactional
-    public List<Employee> getAllEmployee() {
+    public List<Employee> getAllEmployees() {
 
         Session session = sessionFactory.getCurrentSession();
         List<Employee> allEmployees = session.createQuery("from Employee ", Employee.class)
--- src/main/java/com/jaroslav/spring/mvc_hibernate_aop/service/EmployeeService.java
+++ src/main/java/com/jaroslav/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -1,9 +1,9 @@
-package com.jaroslav.spring.mvc_hibernate_aop.dao;
+package com.jaroslav.spring.mvc_hibernate_aop.service;
 
 import com.jaroslav.spring.mvc_hibernate_aop.entity.Employee;
 
 import java.util.List;
 
-public interface EmployeeDAO {
+public interface EmployeeService {
     public List<Employee> getAllEmployees();
 }
